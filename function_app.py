cd467176bdf00704776f69ca3ee05f9d365168b4
Diyan Ivanov <coldys_@hotmail.com>
2026-08-15 20:58:44 +0100

    feat: add service health check

--- function_app.py
+++ function_app.py
@@ -3,4 +3,18 @@ import datetime
 import json
 import logging
 
-app = func.FunctionApp()
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route='health', methods=['GET'])
+def health(req: func.HttpRequest) -> func.HttpResponse:
+    res = {
+        'status': 'ok',
+        'service': 'sig-app',
+        'description': 'Health check for Serverless Invoice Generator API',
+        'version': '0.1'
+    }
+    return func.HttpResponse(
+        json.dumps(res),
+        mimetype="application/json",
+        status_code=200
+    )
