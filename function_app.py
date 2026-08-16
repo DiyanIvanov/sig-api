9cc58bd800b6a756efe3b3f9dc831031e4266727
Diyan Ivanov <coldys_@hotmail.com>
2026-08-16 08:37:16 +0100

    feat: add invoice endpoint

--- function_app.py
+++ function_app.py
@@ -3,6 +3,9 @@ import datetime
 import json
 import logging
 
+from models.invoice import Invoice
+from pydantic import ValidationError
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route='health', methods=['GET'])
@@ -17,4 +20,42 @@ def health(req: func.HttpRequest) -> func.HttpResponse:
         json.dumps(res),
         mimetype="application/json",
         status_code=200
+    )
+
+@app.function_name('invoices')
+@app.route(route='invoices', methods=['POST'])
+def invoices(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        req_body = req.get_json()
+        invoice = Invoice.model_validate(req_body)
+
+    except ValidationError as e:
+        errors = []
+        for err in e.errors():
+            location = ' -> '.join(str(loc) for loc in err['loc'])
+            errors.append(location)
+
+
+        return func.HttpResponse(
+            json.dumps({'message': 'This field is required.','errors': errors}),
+            status_code=422,
+            mimetype='application/json',
+        )
+
+    except ValueError:
+        return func.HttpResponse(
+            'Invalid JSON',
+            status_code=422
+        )
+
+    output = {
+        'invoice_id': invoice.invoice_id,
+        'invoice_date': invoice.invoice_date,
+        'invoice_url': ''
+    }
+
+    return func.HttpResponse(
+        json.dumps(output),
+        mimetype="application/json",
+        status_code=201
     )
