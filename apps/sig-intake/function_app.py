b05210007b7144c5f986a3f0fef7077bb7eb9c8d
Diyan Ivanov <coldys_@hotmail.com>
2026-08-31 09:46:22 +0100

    feat: add SIG Intake health check end point

--- apps/sig-intake/function_app.py
+++ apps/sig-intake/function_app.py
@@ -3,4 +3,15 @@ import datetime
 import json
 import logging
 
-app = func.FunctionApp()
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.function_name('health')
+@app.route(route='/health', methods=['GET'])
+def health(req: func.HttpRequest) -> func.HttpResponse:
+    res = {
+        'status': 'ok',
+        'service': 'sig-intake',
+        'description': 'Health check',
+        'version': '0.1'
+    }
+    return func.HttpResponse(json.dumps(res), mimetype='application/json', status_code=200)
