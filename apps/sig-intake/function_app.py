1f2eed357e23b290864fa12c9cc343c9981ffbdc
Diyan Ivanov <coldys_@hotmail.com>
2026-09-01 20:36:20 +0100

    feat: add invoice generation from CSV data

--- apps/sig-intake/function_app.py
+++ apps/sig-intake/function_app.py
@@ -2,6 +2,9 @@ import azure.functions as func
 import datetime
 import json
 import logging
+from exceptions import CSVParseError, CSVValidationError
+from models import Invoice
+from services import csv_to_dict
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -15,3 +18,29 @@ def health(req: func.HttpRequest) -> func.HttpResponse:
         'version': '0.1'
     }
     return func.HttpResponse(json.dumps(res), mimetype='application/json', status_code=200)
+
+
+@app.function_name('generate_invoices')
+@app.route(route='/generate_invoices', methods=['POST'])
+def generate_invoices(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        data = req.files['file']
+    except KeyError:
+        return func.HttpResponse(
+            'No CSV file found',
+            status_code=422,
+            mimetype='application/json'
+        )
+
+    try:
+        res = csv_to_dict(data)
+    except (CSVValidationError, CSVParseError) as e:
+        return func.HttpResponse(
+            str(e),
+            status_code=422,
+            mimetype='application/json'
+        )
+
+    invoice = Invoice.model_validate(res)
+
+    return func.HttpResponse(json.dumps(invoice.dict()), mimetype='application/json', status_code=200)
